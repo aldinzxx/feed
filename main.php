a141e4889125af1e463ecb4a7e98043cbdc19c99
aldinzxx <114569390+aldinzxx@users.noreply.github.com>
2022-09-28 15:28:32 +0700

    Create main.php

---
+++ main.php
@@ -0,0 +1,20 @@
+<?php
+function metersToFeetInches($meters, $echo = true)
+{
+    $m = $meters;
+    $valInFeet = $m*3.2808399;
+    $valFeet = (int)$valInFeet;
+    $valInches = round(($valInFeet-$valFeet)*12);
+    $data = $valFeet."&prime;".$valInches."&Prime;";
+    if($echo == true)
+    {
+            echo $data;
+    } else {
+            return $data;
+    }
+}
+?>
+<?php
+$feetInches = metersToFeetInches(1.75,false);
+echo $feetInches;
+?>
